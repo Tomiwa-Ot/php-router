b3d8ec880c58d98dcec67036588caff94f4a27cc
Olorunfemi-Ojo Tomiwa <ot.server1@outlook.com>
2022-07-11 20:35:08 +0100

    env parser

--- src/Library/Config.php
+++ src/Library/Config.php
@@ -1,9 +1,47 @@
 <?php
 
+/**
+*   Parse and return .env configuration properties
+*/
+
 class Config
 {
-    public function __construct()
+
+    /**
+     *  Project base directory
+    */
+    public static $baseDir = __DIR__;
+
+
+    /**
+    *   Serializes the properties of .env in array format
+    */
+    private static function parseEnvProperties()
     {
-        
+        $envProperties = array();
+        $vars = file(Config::$baseDir . '/.env', FILE_IGNORE_NEW_LINES);
+        foreach($vars as $var)
+        {
+            $envProperties[explode('=', $var)[0]] = explode('=', $var)[1];
+        }
+        return $envProperties;
     }
+
+
+    /**
+    *   Return .env property
+    */
+    public static function getEnvProperties($property = '')
+    {
+        $envProperties = Config::parseEnvProperties();
+        if(empty($property))
+        {
+            return $envProperties;
+        } 
+        else 
+        {
+            return $envProperties[$property];
+        }
+    }
+
 }
