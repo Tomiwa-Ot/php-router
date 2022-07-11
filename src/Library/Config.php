1b75acce998cbde73c7130343a80bd67ff8a4581
Olorunfemi-Ojo Tomiwa <ot.server1@outlook.com>
2022-07-11 20:57:54 +0100

    made changes

--- src/Library/Config.php
+++ src/Library/Config.php
@@ -1,27 +1,28 @@
 <?php
 
 /**
-*   Parse and return .env configuration properties
-*/
+ *   Parse and return .env configuration properties
+ */
 
 class Config
 {
 
     /**
      *  Project base directory
-    */
+     */
     public static $baseDir = __DIR__;
 
 
     /**
-    *   Serializes the properties of .env in array format
-    */
+     *   Serializes the properties of .env in array format
+     */
     private static function parseEnvProperties()
     {
         $envProperties = array();
         $vars = file(Config::$baseDir . '/.env', FILE_IGNORE_NEW_LINES);
         foreach($vars as $var)
         {
+            if(substr($var, 0, 2) === '##') continue;
             $envProperties[explode('=', $var)[0]] = explode('=', $var)[1];
         }
         return $envProperties;
@@ -29,8 +30,8 @@ class Config
 
 
     /**
-    *   Return .env property
-    */
+     *   Return .env property
+     */
     public static function getEnvProperties($property = '')
     {
         $envProperties = Config::parseEnvProperties();
