8d217f78d1b186751211facee4ff3e63750959c2
Olorunfemi-Ojo Tomiwa <ot.server1@outlook.com>
2022-07-12 20:23:00 +0100

    Create BaseController.php

---
+++ src/Controller/BaseController.php
@@ -0,0 +1,36 @@
+<?php
+
+require __DIR__ . '/../Library/Response.php';
+
+class BaseController extends Response
+{
+    public function get()
+    {
+
+    }
+
+    public function post()
+    {
+
+    }
+
+    public function put()
+    {
+
+    }
+
+    public function delete()
+    {
+
+    }
+
+    public function head()
+    {
+
+    }
+
+    public function options()
+    {
+
+    }
+}
