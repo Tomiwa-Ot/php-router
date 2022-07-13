f5110ee17f205160002111f92f3fa050337b2c19
Olorunfemi-Ojo Tomiwa <ot.server1@outlook.com>
2022-07-13 15:13:37 +0100

    Update Response.php

--- src/Library/Response.php
+++ src/Library/Response.php
@@ -1,41 +1,42 @@
 <?php
 
 /**
- *  HTTP Response Class
+ *  HTTP Response Functions
  */
 
-class Response
+
+ $reqVar = array();
+
+ 
+/**
+ *  Returns json output
+ */
+function json($data, $statusCode)
 {
+    header('Content-Type: application/json; charset=utf-8');
+    http_response_code($statusCode);
+    echo json_encode($data);
+}
 
-    /**
-     *  Returns json output
-     */
-    public function json($data, $statusCode)
-    {
-        header('Content-Type: application/json; charset=utf-8');
-        echo json_encode($data);
-        http_response_code($statusCode);
-    }
+/**
+ *  Returns xml output
+ */
+function xml($data, $statusCode)
+{
+    $data = array($data);
+    header('Content-Type: application/xml');
+    http_response_code($statusCode);
+    $xml = new SimpleXMLElement('<root/>');
+    array_flip($data);
+    array_walk_recursive($data, array($xml, 'addChild'));
+    echo $xml->asXML();
+}
 
-    /**
-     *  Returns xml output
-     */
-    public function xml($data = array(), $statusCode)
-    {
-        header('Content-Type: application/xml');
-        $xml = new SimpleXMLElement('<root/>');
-        array_flip($data);
-        array_walk_recursive($data, array($xml, 'addChild'));
-        echo $xml->asXML();
-        http_response_code($statusCode);
-    }
-
-    /**
-     *  Render html view
-     */
-    public function render($view, $data = array())
-    {
-        if(count($data)) extract($data);
-        require __DIR__ . '/../View/layout/' . $view;
-    }
+/**
+ *  Render html view
+ */
+function render($view, $data = array())
+{
+    if(count($data)) extract($data);
+    require __DIR__ . '/../View/layout/' . $view;
 }
