028ad0af09cf09ae8575371bce5899ebb82e8137
Olorunfemi-Ojo Tomiwa <ot.server1@outlook.com>
2022-07-13 15:14:05 +0100

    Update BaseController.php

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -6,38 +6,7 @@
 
 require_once __DIR__ . '/../Library/Response.php';
 
-class BaseController extends Response
+class BaseController
 {
 
-    /**
-     *  On HTTP GET
-     */
-    public static function get()
-    {
-
-    }
-
-    /**
-     *  On HTTP POST
-     */
-    public static function post()
-    {
-
-    }
-
-    /**
-     *  On HTTP PUT
-     */
-    public static function put()
-    {
-
-    }
-
-    /**
-     *  On HTTP DELETE
-     */
-    public static function delete()
-    {
-
-    }
 }
