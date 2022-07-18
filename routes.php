91ab4caa4058f4312cf33787fa27539b003cdbe8
Olorunfemi-Ojo Tomiwa <ot.server1@outlook.com>
2022-07-18 15:47:09 +0100

    Update routes.php

--- routes.php
+++ routes.php
@@ -2,8 +2,7 @@
 
 require_once __DIR__ . '/Library/Router.php';
 
-foreach (glob(__DIR__ . '/Controller/*.php') as $file)
-{
+foreach (glob(__DIR__ . '/Controller/*.php') as $file) {
     if ($file === __DIR__ . '/Controller/BaseController.php') continue;
     require_once $file;
 }
